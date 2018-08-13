3bcb43c83392234e81acf460ea07500019ff0f64
Keshia White <keshiamwhite@gmail.com>
2018-08-13 01:40:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,19 +18,6 @@ function logWhisper(string) {
   string.toLowerCase()
 }
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
 
  function sayHiToGrandma(string){
  if (string.toUpperCase)
